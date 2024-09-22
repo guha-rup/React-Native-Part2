9bb1fb8748e2e4e5049262f5d68303e26723a48c
guha-rup <guharup105@.com>
2024-09-22 09:17:24 +0530

    View, Text, Button

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Button title='Press Here'/>  
+    </View>
+  )
+}
+
+export default App
